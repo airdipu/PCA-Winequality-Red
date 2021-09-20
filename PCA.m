fb84710eba7933dcf0de94d7fdf7317ba1a59984
airdipu <airdipu@gmail.com>
2021-09-20 21:36:20 +0300

    updated code

--- PCA.m
+++ PCA.m
@@ -1,7 +1,7 @@
 data = csvread('winequality-red.csv', 1, 0); % Loading the dataset without header
 TF = ismissing(data); % checking the missing data
 sum(TF);
-ndata = zscore(data);
+ndata = zscore(data); % Standaraization 
 plot(ndata); % checking data center so the mean of the data stands in the origin
 [Loadings, Scores, EigenVals, T2, Explained, mu] = pca(ndata); % Simple PCA
 
@@ -12,4 +12,19 @@ vbls = {'fixedAcidity', 'volatileAcidity', 'citricAcid', 'residualSugar',...
 biplot(Loadings(:,1:2), 'Scores', Scores(:,1:2),...
      'VarLabels', vbls);
 idx = find(cumsum(Explained)>95);
+% Recomputation having the number of components
+[Loadings2, Scores2] = pca(ndata, 'NumComponents', 6);
+
+% Some predictors are more important than others
+[rows, cols] = size(ndata);
+weights = ones(cols,1);
+weight(3) = 18;
+[Loadings3, Scores3] = pca(ndata, 'VariableWeights', weights);
+
+% ploting the T2 value
+figure;
+a = 1:length(T2);
+plot(a, T2); % To identify the outliers
+xlabel('Sample');
+ylabel('T square scores')
 
